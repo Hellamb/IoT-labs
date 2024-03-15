101fe3a3e1d54fd0e5b9a7ab86a94123c2551585
bohdin <117035076+bohdin@users.noreply.github.com>
2024-03-15 22:22:23 +0200

    lab3

--- hub/app/adapters/store_api_adapter.py
+++ hub/app/adapters/store_api_adapter.py
@@ -10,8 +10,10 @@ from app.interfaces.store_gateway import StoreGateway
 
 
 class StoreApiAdapter(StoreGateway):
-    def __init__(self, api_base_url):
+    def __init__(self, api_base_url, buffer_size=10):
         self.api_base_url = api_base_url
+        self.buffer_size = buffer_size
+        self.buffer = []
 
     def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
         """
@@ -22,3 +24,77 @@ class StoreApiAdapter(StoreGateway):
             bool: True if the data is successfully saved, False otherwise.
         """
         # Implement it
+        try:
+            # Add the batch of data to the buffer
+            self.buffer.extend(processed_agent_data_batch)
+
+            # Check if the buffer size has been reached
+            if len(self.buffer) >= self.buffer_size:
+                # If buffer size is reached, send the data
+                success = self.send_data(self.buffer)
+                # Clear the buffer after sending
+                self.buffer.clear()
+                return success
+            else:
+                return True
+        except Exception as e:
+            logging.error(f"Error occurred: {e}")
+            return False
+
+    def __processed_agent_data_list_to_list_of_dict(
+        self, processed_agent_data_list: List[ProcessedAgentData]
+    ):
+        """
+        Convert a list of ProcessedAgentData objects into a list of dictionaries.
+        Parameters:
+            processed_agent_data_list (List[ProcessedAgentData]): List of ProcessedAgentData objects.
+        Returns:
+            List[dict]: List of dictionaries representing the processed agent data.
+        """
+        processed_data = []
+        for data in processed_agent_data_list:
+            # Convert each ProcessedAgentData object to a dictionary and append to the list
+            processed_data.append(data.dict())
+
+        # Convert the timestamp of each agent data to ISO format
+        for data in processed_data:
+            data["agent_data"]["timestamp"] = data["agent_data"][
+                "timestamp"
+            ].isoformat()
+
+        return processed_data
+
+    def send_data(self, data: List[ProcessedAgentData]) -> bool:
+        """
+        Send the processed agent data to the Store API.
+        Parameters:
+            data (List[ProcessedAgentData]): List of processed agent data.
+        Returns:
+            bool: True if the data is successfully saved, False otherwise.
+        """
+        # Construct the URL for sending data
+        url = f"{self.api_base_url}/processed_agent_data/"
+        try:
+            # Convert the data to a list of dictionaries
+            json_data = self.__processed_agent_data_list_to_list_of_dict(data)
+            # Convert the JSON data to a string
+            json_data_str = str(json.dumps(json_data, indent=4))
+
+            # Send a POST request to the API endpoint with JSON data
+            response = requests.post(
+                url, data=json_data_str, headers={"Content-Type": "application/json"}
+            )
+
+            # Check if the response is successful
+            if response.status_code >= 200 and response.status_code < 300:
+                return True
+            else:
+                # Log if the request fails
+                logging.error(
+                    f"Failed to save data. Status code: {response.status_code}, URL: {url}"
+                )
+                return False
+        except Exception as e:
+            # Log and handle exceptions
+            logging.error(f"Error occurred: {e}")
+            return False
--- hub/tests/test_store_api_adapter.py
+++ hub/tests/test_store_api_adapter.py
@@ -1,14 +1,19 @@
+import datetime
+from datetime import UTC
 import requests
 import unittest
 from unittest.mock import Mock, patch
+from pydantic_core._pydantic_core import TzInfo
 from app.adapters.store_api_adapter import StoreApiAdapter
 from app.entities.agent_data import AccelerometerData, AgentData, GpsData
 from app.entities.processed_agent_data import ProcessedAgentData
+
 
 class TestStoreApiAdapter(unittest.TestCase):
     def setUp(self):
         # Create the StoreApiAdapter instance
         self.store_api_adapter = StoreApiAdapter(api_base_url="http://test-api.com")
+
     @patch.object(requests, "post")
     def test_save_data_success(self, mock_post):
         # Test successful saving of data to the Store API
@@ -31,13 +36,14 @@ class TestStoreApiAdapter(unittest.TestCase):
         mock_response = Mock(status_code=201)  # 201 indicates successful creation
         mock_post.return_value = mock_response
         # Call the save_data method
-        result = self.store_api_adapter.save_data(processed_data)
+        result = self.store_api_adapter.save_data([processed_data])
         # Ensure that the post method of the mock is called with the correct arguments
         mock_post.assert_called_once_with(
-            "http://test-api.com/agent_data", json=processed_data.model_dump()
+            "http://test-api.com/agent_data", json=[processed_data.model_dump()]
         )
         # Ensure that the result is True, indicating successful saving
         self.assertTrue(result)
+
     @patch.object(requests, "post")
     def test_save_data_failure(self, mock_post):
         # Test failure to save data to the Store API
@@ -60,13 +66,14 @@ class TestStoreApiAdapter(unittest.TestCase):
         mock_response = Mock(status_code=400)  # 400 indicates a client error
         mock_post.return_value = mock_response
         # Call the save_data method
-        result = self.store_api_adapter.save_data(processed_data)
+        result = self.store_api_adapter.save_data([processed_data])
         # Ensure that the post method of the mock is called with the correct arguments
         mock_post.assert_called_once_with(
-            "http://test-api.com/agent_data", json=processed_data.model_dump()
+            "http://test-api.com/agent_data", json=[processed_data.model_dump()]
         )
         # Ensure that the result is False, indicating failure to save
         self.assertFalse(result)
+
 
 if __name__ == "__main__":
     unittest.main()
